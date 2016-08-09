6e3d31284019c9f31279e01e9e103f7bf5067ffc
thrivikram <thrivikram@alhazm.com>
2016-08-09 18:46:20 +0300

    First Setup
    First Setup

---
+++ TMock/TMock/Mock.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TMock
+{
+    public class Mock<T>
+    {
+        private T _object;
+        public T Object
+        {
+            get
+            {
+                return _object;                
+            }
+        }
+
+        public Mock<T>SetUp<TResult>(Expression<Func<T, TResult>> f)
+        {
+            if (!typeof(T).IsInterface)
+            {
+                throw new InvalidOperationException(typeof(T).Name
+                    + " is not an interface");
+            }
+
+            return this;
+        } 
+    }
+}
---
+++ TMock/TMockTests/MockUnitTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TMock;
+
+namespace TMockTests
+{
+    [TestClass]
+    public class MockUnitTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var mock = new Mock<ITest>();
+            mock.SetUp(f => f.Add(1, 2));
+        }
+    }
+
+    public interface ITest
+    {
+        int Add(int i, int j);
+    }
+}
